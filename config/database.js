08eaab5b4f8ae932d96570c651f4d775fe54b694
zia123123 <ziamuhamad@gmail.com>
2021-10-20 23:39:56 +0700

    first commit

--- config/database.js
+++ config/database.js
@@ -5,18 +5,18 @@ module.exports = {
   //Configuracion de DB
 
 
-  username: process.env.DB_USERNAME || "root",
-  password: process.env.DB_PASSWORD ||  "",
-  database: process.env.DB_DATABASE || "alihsan",
-  host: process.env.DB_HOST ||"127.0.0.1",
-  dialect: process.env.DB_DIALECT || "mysql",
-
-
-  // username: process.env.DB_USERNAME || "ziakasep",
-  // password: process.env.DB_PASSWORD ||  "@Zia123123",
+  // username: process.env.DB_USERNAME || "root",
+  // password: process.env.DB_PASSWORD ||  "",
   // database: process.env.DB_DATABASE || "alihsan",
-  // host: process.env.DB_HOST ||"45.13.132.252",
+  // host: process.env.DB_HOST ||"127.0.0.1",
   // dialect: process.env.DB_DIALECT || "mysql",
+
+
+  username: process.env.DB_USERNAME || "ziakasep",
+  password: process.env.DB_PASSWORD ||  "@Zia123123",
+  database: process.env.DB_DATABASE || "alihsan",
+  host: process.env.DB_HOST ||"45.13.132.252",
+  dialect: process.env.DB_DIALECT || "mysql",
 
 
   
